5d9c9a43d8491d4c226bb2c251183423c58e5500
Aarika Turner <turneraarika@gmail.com>
2021-02-12 13:52:58 -0500

    push updates

--- index.js
+++ index.js
@@ -7,38 +7,31 @@
 
 
 // This is setup for the shop page
-// var shop = [
-//   {
-//     title: 'Beds',
-//     image: "",
-//     price: '$7.50 and up',
-//     description: "It's a pet bed"
-//   }
-//   ]
-//   var postHTML = ""
-//   for (var i = 0; i < shop.length; i++){
-//     var container = '<div class="container" ' + '>'
-//       var heading = '<div class=" ' + '"><h2>' + shop[i].title + '</h2>'
-//       var image = '<img src="' + shop[i].image + '"/>'
-//       var price = '<p> $' + shop[i].price + '</p>'
-//       var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="">add to cart</button></div></div>'
-//       var concatThis = heading + image + price + description;
-//       postHTML = postHTML + concatThis
-//   }
-//   document.getElementById('market').innerHTML = postHTML
 
   var shop = [
     {
-      title: "Beds",
+      title: "Dog Banana Pajamas",
       image: "./Images/dogBanana.jpg",
-      price: "$39.95",
-      description: "Plush oversized pet bed"
+      price: "39.95",
+      description: "Cute, comfy, and stylish pajamas."
+    },
+    {
+      title: "Pet Champion Hoodie",
+      image: "./Images/dogHoodie.jpg",
+      price: "39.95",
+      description: "Stylish and warm pet hoodie."
+    },
+    {
+      title: "Pet Block Color T-Shirt",
+      image: "./Images/dogShirt.jpg",
+      price: "39.95",
+      description: ""
     }
   ];
   var postHTML = ""
   for (var i = 0; i < shop.length; i++){
     var container = "<div class='card'>" 
-    var heading = '<div class=" card=body" ' + '"><h2>' + shop[i].title + '</h2>'
+    var heading = '<div class=" card=body" ' + '"><h3>' + shop[i].title + '</h3>'
       var image = '<img height=\'150px\' width=\'150px\' src="' + shop[i].image + '"/>'
       var price = '<p> $' + shop[i].price + '</p>'
       var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="">add to cart</button></div></div>'
@@ -47,3 +40,5 @@
 
   };
   document.getElementById('market').innerHTML = postHTML
+
+  
