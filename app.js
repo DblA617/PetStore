7b02703c3c7b31764b85ff7c6b95fa5b3a5533df
Aarika Turner <turneraarika@gmail.com>
2021-02-12 03:01:59 -0500

    push updates

--- app.js
+++ app.js
@@ -7,22 +7,43 @@ var carousel = new bootstrap.Carousel(myCarousel, {
 
 
 // This is setup for the shop page
-var shop = [
-  {
-    title: 'Beds',
-    image: '',
-    price: '$7.50 and up',
-    description: "It's a pet bed"
-  }
+// var shop = [
+//   {
+//     title: 'Beds',
+//     image: "",
+//     price: '$7.50 and up',
+//     description: "It's a pet bed"
+//   }
+//   ];
+//   var postHTML = ""
+//   for (var i = 0; i < shop.length; i++){
+//     var container = '<div class="container" ' + '>'
+//       var heading = '<div class=" ' + '"><h2>' + shop[i].title + '</h2>'
+//       var image = '<img src="' + shop[i].image + '"/>'
+//       var price = '<p> $' + shop[i].price + '</p>'
+//       var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="">add to cart</button></div></div>'
+//       var concatThis = heading + image + price + description;
+//       postHTML = postHTML + concatThis
+//   }
+//   document.getElementById('market').innerHTML = postHTML
+
+  var beds = [
+    {
+      title: "Beds",
+      image: "",
+      price: "$39.95",
+      description: "Plush oversized pet bed"
+    }
   ];
-  var postHTML = ""
-  for (var i=0; i < shop.length; i++){
-    var container = '<div class="container" ' + '>'
-      var heading = '<div class=" ' + '"><h2>' + shop[i].title + '</h2>'
+  var innerHTML = ""
+  for (var i = 0; <shop.length; i++){
+    var container = "<div class='container'>"
+    var heading = '<div class=" ' + '"><h2>' + shop[i].title + '</h2>'
       var image = '<img src="' + shop[i].image + '"/>'
       var price = '<p> $' + shop[i].price + '</p>'
       var description = '<div class=""><p>'+ shop[i].description + '</p><button type="button" class="">add to cart</button></div></div>'
       var concatThis = heading + image + price + description;
       postHTML = postHTML + concatThis
-  }
+
+  };
   document.getElementById('market').innerHTML = postHTML
